caf5c2e28454f2852ce7823c6f36796f8b2fb582
PanzerkampfwagenVmit88offensivekkanone <brambora.eu@gmail.com>
2024-11-22 16:44:27 +0100

    a

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from ursina import *
 app = Ursina(development_mode=True, use_ingame_console=True)
 
 heightmap1 = Terrain(heightmap='heightmap.jpg',skip=1)
-MAPA = Entity(model=heightmap1, texture='kanone.jpg', scale=(100,20,100),)
+MAPA = Entity(model=heightmap1, texture='macrowawe.jpg', scale=(100,20,100),)
 
 
 EditorCamera()  # add camera controls for orbiting and moving the camera 

372ef888b03b93a5e9a02089211c7d4991fd109d
PanzerkampfwagenVmit88offensivekkanone <brambora.eu@gmail.com>
2024-11-22 01:45:51 +0100

    ursina

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import math
 import pynput
+from ursina import *
+
+app = Ursina()
+
+cube = Entity(model='sphere', texture='macrowawe.jpg', scale=2, collider='box')
+
+def spin():
+    cube.animate('rotation_y', cube.rotation_y+720, duration=5, curve=curve.in_out_expo)
+
+
+cube.on_click = spin
+EditorCamera()  # add camera controls for orbiting and moving the camera
+
+app.run()
